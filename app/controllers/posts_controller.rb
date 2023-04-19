8d4abadcafd0b7d22132219098f733f07d4b0169
asifaminisonline <asifaminisonline@gmail.com>
2023-04-19 14:13:22 +0530

    fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,21 +12,21 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
-#   def create
-#     @author = User.find(params[:user_id])
-#     @post = @author.posts.new(post_params)
+  def create
+    @author = User.find(params[:user_id])
+    @post = @author.posts.new(post_params)
 
-#     if @post.save
-#       redirect_to user_path(id: @post.author_id), notice: 'Post was successfully created'
+    if @post.save
+      redirect_to user_path(id: @post.author_id), notice: 'Post was successfully created'
 
-#     else
-#       render :new, alert: 'Error ccurred while creating the post'
-#     end
-#   end
+    else
+      render :new, alert: 'Error ccurred while creating the post'
+    end
+  end
 
-#   private
+  private
 
-#   def post_params
-#     params.require(:post).permit(:title, :text)
-#   end
-# end
+  def post_params
+    params.require(:post).permit(:title, :text)
+  end
+end
